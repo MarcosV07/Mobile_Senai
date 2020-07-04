4ee44c50d159ca09d3945e8f58c7872e380c54ea
Marcos Vinicius <vinisantos.1395@gmail.com>
2020-07-04 17:40:55 -0300

    Devflix

---
+++ DeveFlix/src/pages/Home/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {StatusBar} from 'react-native';
+import styled from 'styled-components/native';
+import * as ScreenOrientation from 'expo-screen-orientation';
+
+import VideosList from '../../Database/videos.json';
+import Header from "../../components/Header";
+import Videos from "../../components/Videos";
+import Poster from "../../components/Poster";
+
+const Container = styled.ScrollView`
+    flex: 1;
+    background-color: #333333;
+`;
+
+async function changeScreenOrientation(){
+    await ScreenOrientation.lockAsync(
+        ScreenOrientation.OrientationLock.PORTRAIT
+    );
+}
+
+export default function Home(){
+    changeScreenOrientation();
+    return(
+        <>
+            <StatusBar 
+                translucent 
+                backgroundColor="transparent" 
+                barStyle="light-content"
+            />
+            <Container>
+                <Poster
+                    url={"https://i.ytimg.com/vi/C_3qWjNVbPU/maxresdefault.jpg"}
+                    title={
+                        "Cursos de Tecnologia do SENAI"
+                    }
+                    tags={["React Native", "Expo"]}
+                    content={<Header />}
+                ></Poster>
+                <Videos label="Recomendados" videos={VideosList}> </Videos>
+                <Videos label="Lançamentos" videos={VideosList}> </Videos>
+                <Videos label="Mais vistos" videos={VideosList}> </Videos>
+            </Container>
+        </>
+    );
+}
+
